30ad81af5a364a8e7b8c06d5b7ff0acb861b48d5
Gabriel Carrizo <gab.carrizo@live.com>
2017-10-04 12:33:18 +0200

    server can now plot

--- tcp.py
+++ tcp.py
@@ -1,15 +1,34 @@
 import socket
+import numpy as np
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 MESSAGES = open("example_pulse.txt","r")
-MESSAGE = MESSAGES.read(BUFFER_SIZE)
+MESSAGE = MESSAGES.read()
+
+#control variables & list
+control = 1
+idx = 0
+MESSAGES = []
+prev_idx = 0
+
+#divide the "messages" into buffer_size pieces
+for idx in range(BUFFER_SIZE,len(MESSAGE),BUFFER_SIZE):
+    MESSAGES.append(MESSAGE[prev_idx:idx])
+    prev_idx = idx
+MESSAGES.append(MESSAGE[prev_idx:len(MESSAGE)])
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
 
-print "received data:", data
+check = 0
+
+for line in MESSAGES:
+    s.send(line)
+    check += len(line)
+    data = s.recv(BUFFER_SIZE)
+    print "received data:", data
+print check
+s.close()
--- tcp_serv.py
+++ tcp_serv.py
@@ -1,13 +1,18 @@
 import socket
-
+import matplotlib.pyplot as plt
+import re
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+no_of_packages =0
+packages = []
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
+
+
 
 conn, addr = s.accept()
 print 'Connection address:', addr
@@ -16,5 +21,44 @@ while 1:
     if not data: break
     print "received data:", data
     conn.send(data)  # echo
+    no_of_packages +=1
+    packages.append(data)
+conn.close()
 
-#conn.close()
+total_packages = ''.join(packages)
+print len(total_packages)
+print no_of_packages
+
+test = total_packages.split('\n')
+
+signal = []
+mean = []
+bpm = []
+time = []
+order = []
+
+
+for line in test:
+    split_line = line.replace('u','').replace('\'','').replace(',','').replace(')','').replace('(','')
+    split_line = split_line.split(' ')
+    if len(split_line)>1:
+        print split_line
+        signal.append(split_line[0])
+        mean.append(split_line[1])
+        bpm.append(split_line[2])
+        time.append(split_line[3])
+        order.append(split_line[4])
+
+end_of_time=len(time)
+plt.figure()
+
+plt.plot(time[end_of_time-200:end_of_time-1],signal[end_of_time-200:end_of_time-1])
+plt.plot(time[end_of_time-200:end_of_time-1],mean[end_of_time-200:end_of_time-1])
+plt.title(bpm[end_of_time-1])
+plt.show()
+
+
+
+#print test
+print len(test)
+print test[0]
